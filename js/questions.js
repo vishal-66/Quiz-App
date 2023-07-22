931e0d1a1684dd7b80fd8a4edc5e07ae82f84118
vishal-66 <vishalsighaniya250@gmail.com>
2023-07-22 16:32:28 +0530

    upload javascript file

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+  {
+  numb: 5,
+  question: "What does XML stand for?",
+  answer: "eXtensible Markup Language",
+  options: [
+    "eXtensible Markup Language",
+    "eXecutable Multiple Language",
+    "eXTra Multi-Program Language",
+    "eXamine Multiple Language"
+  ]
+  },
+  {
+    numb: 6,
+    question: "ISP stands for:?",
+    answer: "Internet Service Provider",
+    options: [
+      "(A) Internet Survey Period",
+      "(B) Integrated Service Provider",
+      "(C) Internet Security Protocol",
+      "(D) Internet Service Provider"
+    ]
+    },
+    {
+      numb: 7,
+      question: " Internet Explorer is a:?",
+      answer: "(B) Web Browser",
+      options: [
+        "(A) Any person browsing the net",
+        "(B) Web Browser",
+        "(C) Graphing Package",
+        "(D) News Reader"
+      ]
+      },
+      {
+        numb: 8,
+        question: "The 'http' you type at the beginning of any site's address stands for:?",
+        answer: "(D) Hyper Text Transfer Protocol",
+        options: [
+          "A) HTML Transfer Technology Process",
+         "(B) Hyperspace Terms and Tech Protocol",
+         "(C) Hyperspace Techniques & Tech Progress",
+         "(D) Hyper Text Transfer Protocol"
+          
+        ]
+        },
+        {
+          numb: 9,
+          question: "What was the first ARPANET message?",
+          answer: "(A) 'lo'",
+          options: [
+            "(A) 'lo'",
+            "(B) 'hello world'",
+            "(C) 'mary had a little lamb'",
+            "(D) 'cyberspace, the final frontier'"
+          ]
+          },
+          {
+            numb: 10,
+            question: "What is the difference between the Internet and an intranet?",
+            answer: "(A) One is public, the other is private",
+            options: [
+              "(A) One is public, the other is private",
+              "(B) One is safer than the other",
+              "(C) One can be monitored, the other can't",
+              "(D) None of the above"
+            ]
+            },
+];
